5cf6f5879df19e7d5144472920e5354db82f9f57
baldosa <mast3er@gmail.com>
2018-09-23 10:52:01 -0300

    dot-files.sh

---
+++ dot-files.sh
@@ -0,0 +1,74 @@
+#/bin/bash
+
+#zshrc
+#polybar
+#i3
+#dunstrc
+#wallpaper
+#urxvt
+#lightdm
+#easystroke
+#
+
+I3CONFIG_DIR="$HOME/.config/i3"
+I3CONFIG="$HOME/.config/i3/config"
+DUNSTRC_DIR="$HOME/.config/dunst"
+DUNSTRC="$HOME/.config/dunst/dunstrc"
+ZSHRC="$HOME/.zshrc"
+XRESOURCES="$HOME/.Xresources"
+POLYBAR_DIR="$HOME/.config/polybar"
+POLYBAR="$HOME/.config/polybar/config"
+MPV_DIR="$HOME/.config/mpv"
+MPV="$HOME/.config/mpv/mpv.conf"
+EASYSTROKE="$HOME/.easystroke"
+
+echo $I3CONFIG
+echo $I3CONFIG_DIR
+echo $DUNSTRC
+echo $ZSHRC
+echo $XRESOURCES
+echo $POLYBAR
+echo $POLYBAR_DIR
+echo $MPV
+echo $EASYSTROKE
+
+if [ -d "$I3CONFIG_DIR"  ] || [ -d "$I3CONFIG"  ] ; then
+	rm -rf "$I3CONFIG_DIR"
+fi
+
+if [ -d "$DUNSTRC_DIR"  ] || [ -d "$DUNSTRC"  ] ; then
+	rm -rf "$DUNSTRC_DIR"
+fi
+
+if [ -d "$ZSHRC"  ] ; then
+	rm "$ZSHRC"
+fi
+
+if [ -d "$XRESOURCES"  ] ; then
+	rm "$XRESOURCES"
+fi
+
+if [ -d "$POLYBAR_DIR"  ] || [ -d "$POLYBAR"  ] ; then
+	rm -rf "$POLYBAR_DIR"
+fi
+
+if [ -d "$MPV_DIR"  ] || [ -d "$MPV"  ] ; then
+	rm -rf "$MPV_DIR"
+fi
+
+if [ -d "$EASYSTROKE"  ] ; then
+	rm -rf "$EASYSTROKE"
+fi
+
+
+mkdir $I3CONFIG_DIR
+mkdir $POLYBAR_DIR
+mkdir $MPV_DIR
+mkdir $DUNSTRC_DIR
+ln -s $HOME/dot-files/.i3config $I3CONFIG
+ln -s $HOME/dot-files/.dunstrc $DUNSTRC
+ln -s $HOME/dot-files/.zshrc $ZSHRC
+ln -s $HOME/dot-files/.Xresources $XRESOURCES
+ln -s $HOME/dot-files/.mpv $MPV
+ln -s $HOME/dot-files/.easystroke $EASYSTROKE
+ln -s $HOME/dot-files/.polybar $POLYBAR
---
+++ wallpaper.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#while true; do
+#  feh --randomize --bg-fill ~/wallpapers/op/*
+#  sleep 3600
+#done
+
+feh --bg-scale --no-xinerama ~/dot-files/wallpapers/colors.jpg
